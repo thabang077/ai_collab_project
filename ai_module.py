3682a5549aaece7e0d0431bd7a39a2ae437bf4bc
Sadananda29 <sadanandapv@gmail.com>
2026-03-17 13:27:49 +0530

    code

--- ai_module.py
+++ ai_module.py
@@ -2,10 +2,15 @@ import requests
 
 def ai_agent(question):
     url = "https://api.chucknorris.io/jokes/random"
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["value"]
-    else:
-        return "AI service unavailable"
+    try:
+        response = requests.get(url, timeout=5)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data["value"]
+        else:
+            return "AI service unavailable"
+
+    except Exception as e:
+        return "Error connecting to AI service"
